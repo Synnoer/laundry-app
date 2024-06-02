353d47b9ab89a03892f1693c5aadf292c0ff67df
NaiveEmiya <113496821+AhmadFaikar@users.noreply.github.com>
2024-06-03 02:03:21 +0700

    update orderlist

--- resources/views/admin/orderlist.blade.php
+++ resources/views/admin/orderlist.blade.php
@@ -9,14 +9,15 @@
                             <table class="table-auto w-full">
                                 <thead>
                                     <tr>
-                                        <th class="px-4 py-2">Order Date</th>
-                                        <th class="px-4 py-2">Completion Date</th>
-                                        <th class="px-4 py-2">User</th>
-                                        <th class="px-4 py-2">Products</th>
-                                        <th class="px-4 py-2">Total Weight</th>
-                                        <th class="px-4 py-2">Service</th>
-                                        <th class="px-4 py-2">Fragrance</th>
-                                        <th class="px-4 py-2">Status</th>
+                                        <th class="border px-4 py-2">Order Date</th>
+                                        <th class="border px-4 py-2">Completion Date</th>
+                                        <th class="border px-4 py-2">User</th>
+                                        <th class="border px-4 py-2">Products</th>
+                                        <th class="border px-4 py-2">Total Weight</th>
+                                        <th class="border px-4 py-2">Service</th>
+                                        <th class="border px-4 py-2">Fragrance</th>
+                                        <th class="border px-4 py-2">Status</th>
+                                        <th class="border px-4 py-2">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -35,14 +36,26 @@
                                             <td class="border px-4 py-2">{{ $order->total_weight }} g</td>
                                             <td class="border px-4 py-2">{{ $order->service }}</td>
                                             <td class="border px-4 py-2">{{ $order->fragrance }}</td>
-                                            <td class="border px-4 py-2">{{ $order->status }}</td>
-                                            <td class="px-4 py-2">
+                                            @if($order->status==0)
+                                            <td class="border px-4 py-2">Ongoing</td>
+                                            @endif
+                                            @if($order->status==1)
+                                            <td class="border px-4 py-2">Complete</td>
+                                            @endif
+                                            @if($order->status==0)
+                                            <td class="border px-4 py-2">
                                                 <form action="{{ route('admin.nextStatus', $order->id) }}" method="POST">
                                                     @csrf
                                                     @method('patch')
-                                                    <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded">Next</button>
+                                                    <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded">Update</button>
                                                 </form>
                                             </td>
+                                            @endif
+                                            @if($order->status==1)
+                                            <td class="border px-4 py-2">
+                                                    <button class="bg-green-500 text-white px-2 py-1 rounded">Complete</button>
+                                            </td>
+                                            @endif
                                         </tr>
                                     @endforeach
                                 </tbody>
--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -93,60 +93,6 @@
     </x-slot>
 
     <main class="p-6 ">
-<<<<<<< HEAD
-        @if ($errors->any())
-        <script>
-            alert('{{ $errors->first() }}');
-        </script>
-        @endif
-        <div>
-            <h1 class="text-black text-left mb-6 ">Ongoing Order</h1>
-            @if($ongoingOrders->isNotEmpty())
-                <div class="w-full border-solid border-2  rounded-md border-black bg-white sm:rounded-lg px-4 py-2 grid grid-cols-2">
-                    @foreach($ongoingOrders as $order)
-                        <div>
-                            <img src="/image/Logo-ongoing.png" class="object-center w-9 h-9">
-                        </div>
-                        <div>
-                            <h3 class="border-black px-4 py-2 text-center">
-                                @foreach($order->product_details as $detail)
-                                    {{ $detail['name'] ?? 'Unknown product' }} x {{ $detail['quantity'] ?? 0 }},
-                                @endforeach
-                            </h3>
-                            <div class="grid grid-cols-2 gap-2">
-                                <p class="border-black px-4 py-2">Order : {{ $order->order_date->format('Y-m-d') ?? 'start' }}</p>
-                                <p class="border-black px-4 py-2">Estimate : {{ $order->completion_estimation_date->format('Y-m-d') ?? 'end' }}</p>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @endif
-        </div>
-        <div class="mb-6 mt-6">
-            <h1 class="text-black text-left mb-6">Recent Order</h1>
-            <!-- ganti -->
-            @if($ongoingOrders->isNotEmpty())
-                <div class="w-full border-solid border-2  rounded-md border-black bg-white sm:rounded-lg px-4 py-2 grid grid-cols-2">
-                    @foreach($ongoingOrders as $order)
-                        <div>
-                            <img src="/image/Logo-ongoing.png" class="object-center w-9 h-9">
-                        </div>
-                        <div>
-                            <h3 class="border-black px-4 py-2 text-center">
-                                @foreach($order->product_details as $detail)
-                                    {{ $detail['name'] ?? 'Unknown product' }} x {{ $detail['quantity'] ?? 0 }},
-                                @endforeach
-                            </h3>
-                            <div class="grid grid-cols-2 gap-2">
-                                <p class="border-black px-4 py-2">Order : {{ $order->order_date->format('Y-m-d') ?? 'start' }}</p>
-                                <p class="border-black px-4 py-2">Estimate : {{ $order->completion_estimation_date->format('Y-m-d') ?? 'end' }}</p>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @endif
-        </div>
-=======
     <div>
         <div class="mb-6 mt-6">
         <h1 class="text-black text-left mb-6 ">Ongoing Order</h1>
@@ -196,6 +142,5 @@
             </div>
         @endif
     </div>
->>>>>>> c12c226ada1381c8bb9888c8bde1519d9a1ea340
     </main>    
 </x-app-layout>
