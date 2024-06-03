e6da941afbbcf33d99ff77ad3b4457b0ce109ae5
NaiveEmiya <113496821+AhmadFaikar@users.noreply.github.com>
2024-06-03 14:28:41 +0700

    orderlist

--- resources/views/admin/orderlist.blade.php
+++ resources/views/admin/orderlist.blade.php
@@ -37,23 +37,34 @@
                                             <td class="border px-4 py-2">{{ $order->service }}</td>
                                             <td class="border px-4 py-2">{{ $order->fragrance }}</td>
                                             @if($order->status==0)
-                                            <td class="border px-4 py-2">Ongoing</td>
+                                            <td class="border px-4 py-2">Awaiting Pickup</td>
+                                            @elseif($order->status==1)
+                                            <td class="border px-4 py-2">Order Pickup</td>
+                                            @elseif($order->status==2)
+                                            <td class="border px-4 py-2">Order Arrives At The Laundry</td>
+                                            @elseif($order->status==3)
+                                            <td class="border px-4 py-2">Order is Being Washed</td>
+                                            @elseif($order->status==4)
+                                            <td class="border px-4 py-2">Order Finished Washing</td>
+                                            @elseif($order->status==5)
+                                            <td class="border px-4 py-2">Order Delivered to Destination</td>
+                                            @elseif($order->status==6)
+                                            <td class="border px-4 py-2">Order Arrives at Destination</td>
+                                            @elseif($order->status==7)
+                                            <td class="border px-4 py-2">Completed</td>
                                             @endif
-                                            @if($order->status==1)
-                                            <td class="border px-4 py-2">Complete</td>
-                                            @endif
-                                            @if($order->status==0)
+                                            @if($order->status<=6)
                                             <td class="border px-4 py-2">
                                                 <form action="{{ route('admin.nextStatus', $order->id) }}" method="POST">
                                                     @csrf
                                                     @method('patch')
-                                                    <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded">Update</button>
+                                                    <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded">Next</button>
                                                 </form>
                                             </td>
                                             @endif
-                                            @if($order->status==1)
+                                            @if($order->status==7)
                                             <td class="border px-4 py-2">
-                                                    <button class="bg-green-500 text-white px-2 py-1 rounded">Complete</button>
+                                                    <button class="bg-green-500 text-white px-2 py-1 rounded" disabled>Complete</button>
                                             </td>
                                             @endif
                                         </tr>
